23d292b46edb6c4fe9159ba4e93726f441b57e50
Nikita <hrynts2002@gmail.com>
2024-02-08 17:58:59 +0300

    Added @Min and @Max validation

--- src/main/java/by/hryntsaliou/mvc/Customer.java
+++ src/main/java/by/hryntsaliou/mvc/Customer.java
@@ -1,5 +1,7 @@
 package by.hryntsaliou.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -8,8 +10,12 @@ public class Customer {
     private String firstName;
 
     @NotNull(message = "is required")
-    @Size(min = 1, message = "is required")
+    @Size(min = 1, message = "length should be > 1")
     private String lastName;
+
+    @Min(value = 0, message = "should be >= 0")
+    @Max(value = 10, message = "should be <= 10")
+    private int freePasses;
 
     public Customer() {
 
@@ -29,5 +35,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
