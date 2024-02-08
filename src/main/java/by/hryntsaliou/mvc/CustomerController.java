1cfd2d597561ec5b74744b3c8da02aaa5e204afa
Nikita <hrynts2002@gmail.com>
2024-02-08 17:44:15 +0300

    Added @InitBinder to trim last name whitespaces

--- src/main/java/by/hryntsaliou/mvc/CustomerController.java
+++ src/main/java/by/hryntsaliou/mvc/CustomerController.java
@@ -1,15 +1,25 @@
 package by.hryntsaliou.mvc;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @GetMapping("/")
     public String showForm(Model model) {
@@ -23,6 +33,7 @@ public class CustomerController {
     public String processForm(@Valid @ModelAttribute("customer") Customer customer,
                               BindingResult bindingResult) {
 
+        System.out.println("Last name: |" + customer.getLastName() + "|");
         if (bindingResult.hasErrors()) {
             return "customer-form";
         } else {
